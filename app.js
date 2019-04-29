544c0a7a864d215774284cca198e0c34cadf4d92
ldevernay <boblehamster@yahoo.fr>
2019-04-29 15:17:32 +0200

    edit task

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function addTask() {
     // Créer un élément
-    let task = `<li class="list-group-item">${$("input").val()} <i class="fas fa-trash"></i></li>`;
+    let task = `<li class="list-group-item">${$("input").val()} <i class="fas fa-trash"></i> <i class="fas fa-edit"></i></li>`;
     $("ul").append(task);
     $("input").val("");
 }
@@ -18,3 +18,12 @@ $("input").keypress(function (event) {
 $("ul").on("click", ".fa-trash", function () {
     $(this).parent().remove();
 });
+
+$("ul").on("click", ".fa-edit", function () {
+    $("#text_edit,#edit").remove();
+    $(this).parent().append(`<input type='text' id='text_edit'/><button id='edit'>Modifier</button>`);
+    $("#edit").on("click", function() {
+        console.log($("#text_edit").val());
+        addTask($("#text_edit").val());
+    })
+})
