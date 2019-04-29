75392027c2fc4da8a18749e948ad977f35d6986f
ldevernay <boblehamster@yahoo.fr>
2019-04-29 13:40:32 +0200

    add task w/ keypress

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 function addTask() {
     // Créer un élément
-    let task = `<li class="list-group-item">${$("input").val()}</li>`;
+    let task = `<li class="list-group-item">${$("input").val()} <i class="fas fa-trash"></i></li>`;
     $("ul").append(task);
     $("input").val("");
 }
 
 $("button").on("click", function () {
     addTask();
+});
+
+$("input").keypress(function(event){
+    if(event.which == 13){
+        addTask();
+    }
 });
